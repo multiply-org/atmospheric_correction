862605bc6d1e99870ffaf31aa88ae8390ff7e463
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-06-18 18:24:05 +0200

    changed name

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from setuptools import setup
 
-setup(name='atmospheric_correction',
+setup(name='multiply-atmospheric-correction',
       version='2.0',
       description='Atmospheric correction for Sentinel 2 and Landsat 8',
       author='Feng Yin',
-      packages=['atmospheric_correction'],
+      packages=['multiply_atmospheric_correction'],
      )
